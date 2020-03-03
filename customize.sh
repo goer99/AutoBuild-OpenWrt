88eab0bc085625273e376a47049614dd802cc3c9
goer99 <goer99@users.noreply.github.com>
2020-03-03 19:22:34 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,5 +5,9 @@
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
-#1. Modify default IP
-# sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+
+# 更改默认ip
+# sed -i 's/192.168.1.1/新的ip/g' openwrt/package/base-files/files/bin/config_generate
+
+# 更改主机名
+# sed -i 's/OpenWrt/新的主机名/g' openwrt/package/base-files/files/etc/config/system
