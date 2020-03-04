fab4a0cf4990d7d17c9357743dada593a13185f3
goer99 <goer99@users.noreply.github.com>
2020-03-04 23:27:59 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -10,5 +10,7 @@
 # sed -i 's/192.168.1.1/新的ip/g' openwrt/package/base-files/files/bin/config_generate
 
 # 更改主机名
-#sed -i "s/OpenWrt/$(date -I)/g" openwrt/package/base-files/files/etc/config/system
-sed -i "s/OpenWrt/$(date -I)/g" openwrt/package/base-files/files/bin/config_generate
+#sed -i "s/OpenWrt/新主机名/g" openwrt/package/base-files/files/bin/config_generate
+
+# 更改版本号
+sed -i "s/OpenWrt/Tom build on $(date -I) for /g" openwrt/package/lean/default-settings/files/zzz-default-settings
